4ed116d89c3fea84b8b424d182a5d1311d1cacb8
nicolofabris <nicolo.fabriss@gmail.com>
2023-10-14 15:52:15 +0200

    sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js"
 import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req, res) => {
     const {username, email, password} = req.body
@@ -7,4 +8,15 @@ export const signup = async (req, res) => {
     const newUser = new User({username, email, password})
     await newUser.save()
     res.status(201).json('user created successfully')
+}
+
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body
+    
+    const validUser = await User.findOne({email})
+    if (!validUser) return error 
+
+    const token = jwt.sign({ id: validUser._id}, process.env.JWT_SECRET)
+    res.cookie('access_token', token, {httpOnly: true }).status(200).json(validUser)
+
 }
